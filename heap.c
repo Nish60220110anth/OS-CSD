f9f3fc2853b963851e996f6e330cf3ac350b6236
MasterOogway <kungfupandaunleashed@gmail.com>
2023-10-05 16:09:53 +0530

    complete free heap

--- heap.c
+++ heap.c
@@ -1,24 +1,88 @@
 #include "heap.h"
 
-
 // 1 if not tag found, 0 is found
 int free_block_init(struct free_block* free_head, int tag) {
     struct free_block* pre = free_head;
 
-    while (pre != 0 && pre -> tag != tag) {
-        pre = pre -> next;
+    while (pre != 0 && pre->tag != tag) {
+        pre = pre->next;
     }
 
-    if(pre == 0) return 1;
-    
-}
-
-void free_block_insert(struct free_block* free_head, int size) {
+    if (pre == 0) return 1;
 
 }
 
+/**
+ * \brief inserts a new block at the end of the free list with given size
+ * 
+ * \param free_head the head of the free list
+ * \param size the size of the block to be inserted
+ * 
+ * \returns 0 if successful, 1 else
+ * \note the address of the block is set to last_address
+ * \note the tag of the block is set to free_last
+ * \note the capactity of the block is set to size
+*/
+int free_block_insert(struct free_block* free_head, int size) {
+    if (size == 0) return 1;
+
+    struct free_block* new_blk = (struct free_block*)malloc(sizeof(struct free_block));
+    new_blk->size_free = size; // size blocks got alloted
+    new_blk->address = last_address;
+    last_address += size; // last address got updated for next allocation
+    new_blk->num_procs = 0; // no process is using this block
+    new_blk->tag = free_last; // tag is set to free_last
+    free_last++; // free_last got updated for next allocation
+    new_blk->next = 0; // next is set to 0
+    new_blk->capactity = size;
+
+    if (free_head == 0) {
+        free_head = new_blk;
+        return;
+    }
+
+    struct free_block* pre = free_head;
+    while (pre->next != 0) {
+        pre = pre->next;
+    }
+
+    pre->next = new_blk;
+
+    return 1;
+}
+
+/**
+ * \brief deletes a block with given tag
+ * 
+ * \param free_head the head of the free list
+ * \param tag the tag of the block to be deleted
+ * 
+ * \returns 0 if successful, 1 else
+ * \note remember to free the process block tha used this block using the block_id
+*/
 void free_block_delete(struct free_block* free_head, int tag) {
+    if (free_head == 0) return;
 
+    struct free_block* pre = free_head;
+    struct free_block* pre_pre = 0;
+
+    while (pre != 0 && pre->tag != tag) {
+        pre_pre = pre;
+        pre = pre->next;
+    }
+
+    if (pre == 0) return;
+
+    if (pre_pre == 0) {
+        free_head = pre->next;
+        free(pre);
+        return;
+    }
+
+    pre_pre->next = pre->next;
+    pre_pre->capactity += pre->capactity;
+
+    free(pre);
 }
 
 struct free_block* _free_block_finder(struct free_block* free_head, int size) {
@@ -28,8 +92,8 @@ struct free_block* _free_block_finder(struct free_block* free_head, int size) {
     int best_size = __INT32_MAX__;
     struct free_block* best_block = 0;
     while (pre != 0) {
-        if (pre->size >= size) {
-            int diff = pre->size - size;
+        if (pre->size_free >= size) {
+            int diff = pre->size_free - size;
             if (diff < best_size) {
                 best_size = diff;
                 best_block = pre;
@@ -49,16 +113,37 @@ struct free_block* _free_block_finder(struct free_block* free_head, int size) {
 */
 int free_block_alloc(struct free_block* free_head, int size) {
     struct free_block* _alloted_block = _free_block_finder(free_head, size);
-    if(size == 0) return -1;
-
+    
+    if (size == 0) return -1;
     if (_alloted_block == 0) return -1;
 
-    int address = _alloted_block->address;
+    int used = _alloted_block->capactity - _alloted_block->size_free;
+    int address = _alloted_block->address + used;
+
     _alloted_block->address += size; // size blocks got alloted 
-    _alloted_block->size -= size;
+    _alloted_block->size_free -= size;
     _alloted_block->num_procs++;
 
     return address;
+}
+
+int free_block_dealloc(struct free_block* free_head, int tag, int size) {
+    if (free_head == 0) return 1;
+
+    struct free_block* pre = free_head;
+
+    while (pre != 0 && pre->tag != tag) {
+        pre = pre->next;
+    }
+
+    if (pre == 0) return 1;
+
+    if (pre->size_free + size > pre->capactity) return 1;
+
+    pre->size_free += size;
+    pre->num_procs--;
+
+    return 0;
 }
 
 struct free_block* free_block_at(struct free_block* free_head, int tag) {
@@ -70,11 +155,11 @@ struct free_block* free_block_at(struct free_block* free_head, int tag) {
         pre = pre->next;
     }
 
-    if (pre == 0) return 0;
+    // if (pre == 0) return 0;
     return pre;
 }
 
-int free_block_size(struct free_block* free_head) {
+int free_block_length(struct free_block* free_head) {
     if (free_head == 0) return 0;
     int c = 0;
     struct free_block* pre = free_head;
--- heap.h
+++ heap.h
@@ -2,17 +2,17 @@
 #define INTERNAL_HEAP
 
 int alloc_last = 0, free_last = 0; // tag id's
-int can_use_blocks = 0; // this is address size that free blocks are using 
+int last_address = 0; // this is address size that free blocks are using 
 // remember to use this address with the base address
 
 struct free_block {
     int address; // initial location of the address of the block
-    int size; // size left free in the block
+    int size_free; // size left free in the block
     int capactity; // capactity of the  block
     int tag; // tag id of the block
     int num_procs; // number of processes using the block
     struct free_block* next;
-}*free_head = nullptr, *free_prev = nullptr;
+} *free_head = nullptr, *free_prev = nullptr;
 
 struct proc_block {
     int address; // initial location of the  address of the  block
